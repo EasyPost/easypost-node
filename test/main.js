2c4d31f86c7138135b8573b721cb2f3bc15c9d2e
Jake Epstein <jake@easypost.com>
2014-11-04 15:51:24 -0800

    added tracker to shipment buy response and updated tests

--- test/main.js
+++ test/main.js
@@ -148,9 +148,16 @@ vows.describe("EasyPost API").addBatch({
                     assert.isNull(err);
                     assert.isDefined(response.postage_label);
                     assert.isDefined(response.postage_label.label_url);
+
                     assert.isDefined(response.insurance);
-                    assert.isDefined(response.tracking_code);
                     assert.equal(response.insurance, '249.99');
+
+                    assert.isDefined(response.tracking_code);
+                    assert.equal(response.tracking_code, response.tracker.tracking_code)
+
+                    assert.isDefined(response.tracker);
+                    assert.equal(response.tracker.object, "Tracker")
+                    assert.equal(response.tracker.shipment_id, response.id)
                 }
             }
         }
