131a177d2b8f76e431255a9dc772e0303a2e5c50
Kevin Funk <kevin@easypost.com>
2017-01-18 18:03:26 -0800

    /scan_forms endpoint (api.ScanForm)

---
+++ src/resources/scan_form.js
@@ -0,0 +1,39 @@
+import T from 'proptypes';
+import base from './base';
+import address from './address';
+
+export default (api) => {
+  const Address = address(api);
+
+  return class ScanForm extends base(api) {
+    static _name = 'ScanForm';
+    static url = 'scan_forms';
+
+    static propTypes = {
+      id: T.string,
+      object: T.string,
+      status: T.string,
+      message: T.string,
+      address: T.oneOfType([T.string, T.shape(Address.propTypes)]),
+      tracking_codes: T.arrayOf(T.string),
+      form_url: T.string,
+      form_file_type: T.string,
+      batch_id: T.string,
+      created_at: T.object,
+      updated_at: T.object,
+    }
+
+    static delete() {
+      return this.notImplemented('delete');
+    }
+
+    static wrapJSON(json) {
+      return json;
+    }
+
+    toJSON() {
+      if (this.shipments) return { shipments: this.shipments };
+      return super.toJSON();
+    }
+  };
+};
