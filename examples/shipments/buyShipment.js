18db200d0c4d25b593cc58a6cee9579e901a8e53
Logan Simonsen <32521559+LoganSimonsen@users.noreply.github.com>
2019-01-17 15:37:25 -0700

    Update buyShipment.js

--- examples/shipments/buyShipment.js
+++ examples/shipments/buyShipment.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 const Easypost = require('@easypost/api');
 const api = new Easypost(process.env.testkey);
 
-api.Shipment.retrieve('shp_90f1b68882d34118b20837f81bb8a7f8').then(s => {
-    s.buy('rate_5dcec376eec14f22a64220d6cbb87ec8').then(console.log);
+api.Shipment.retrieve('shp_...').then(s => {
+    s.buy('rate_...').then(console.log);
 });
