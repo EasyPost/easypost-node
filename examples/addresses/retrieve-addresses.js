f663ba8eb9a5c5aafe9bb233482b3c59f7f9ada1
Justintime50 <jhammond@easypost.com>
2020-05-22 12:02:50 -0600

    Added various code examples

---
+++ examples/addresses/retrieve-addresses.js
@@ -0,0 +1,7 @@
+const Easypost = require('@easypost/api');
+
+
+const api = new Easypost(process.env.API_KEY);
+
+const addresses = api.Address.all();
+addresses.then(console.log).catch(console.log);
---
+++ examples/scanforms/create-scanform.js
@@ -0,0 +1,10 @@
+const Easypost = require('@easypost/api');
+
+
+const api = new Easypost(process.env.API_KEY);
+
+const scanform = new api.ScanForm({
+  shipments: ['shp_...'],
+});
+
+scanform.save().then(console.log).catch(console.log);
