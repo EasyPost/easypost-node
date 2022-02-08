4263c4dba93f9e8a98b4550e45ec1c564aa87d96
jchen293 <jchen@easypost.com>
2022-02-08 13:58:36 -0500

    remove retrieveRate() method and its unit tests.

--- src/resources/shipment.js
+++ src/resources/shipment.js
@@ -103,14 +103,6 @@ export default api => (
       return this.rpc('label', { file_format: format }, undefined, 'get');
     }
 
-    async retrieveRates() {
-      this.verifyParameters({
-        this: ['id'],
-      });
-
-      return this.rpc('rates', undefined, undefined, 'get');
-    }
-
     async regenerateRates() {
       this.verifyParameters({
         this: ['id'],
