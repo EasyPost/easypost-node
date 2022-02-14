27bde3ac897ecff69ab89a5ae8764daf9a15e35c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-02-14 13:27:34 -0700

    tests: adds Polly.js VCR tool and fixes coverage
    fix: build by adding mock API key env
    
    fix: build env var line
    
    fix: lint
    
    fix: name function in test file
    
    fix: import name
    
    fix: bump timeout for tests from 2 to 10 seconds
    
    fix: filter auth header bug

---
+++ test/setupPolly.js
@@ -0,0 +1,36 @@
+import FSPersister from '@pollyjs/persister-fs';
+import NodeHttpAdapter from '@pollyjs/adapter-node-http';
+import { resolve } from 'path';
+import { Polly, setupMocha as setupPolly } from '@pollyjs/core';
+
+
+Polly.register(FSPersister);
+Polly.register(NodeHttpAdapter);
+
+function startPolly() {
+  setupPolly({
+    adapters: ['node-http'],
+    persister: 'fs',
+    persisterOptions: {
+      fs: {
+        recordingsDir: resolve(__dirname, './cassettes'),
+      },
+    },
+    recordIfMissing: true,
+    matchRequestsBy: {
+      headers: false,
+    },
+  });
+}
+
+function stripCreds(server) {
+  server.any().on('beforePersist', (_, recording) => {
+    // eslint-disable-next-line no-param-reassign
+    recording.request.headers = recording.request.headers.filter(({ name }) => name !== 'authorization');
+  });
+}
+
+export {
+  startPolly,
+  stripCreds,
+};
