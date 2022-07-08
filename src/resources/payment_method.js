144780c4167b3925e42769b276bc472837c8fb9d
jchen293 <jchen@easypost.com>
2022-07-08 15:12:58 -0400

    addressed feedback

--- src/resources/payment_method.js
+++ src/resources/payment_method.js
@@ -2,12 +2,12 @@ import T from 'proptypes';
 
 export const propTypes = {
   id: T.string,
-  disabled_at: T.string,
   object: T.string,
+  disabled_at: T.string,
   name: T.string,
   last4: T.string,
-  expiration_month: T.string,
-  expiration_year: T.string,
+  expiration_month: T.number,
+  expiration_year: T.number,
   brand: T.string,
   country: T.string,
   verified: T.bool,
