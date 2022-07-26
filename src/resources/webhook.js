1b5ea78da75836630b3a68be6354a359341d422f
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-07-26 09:58:03 -0600

    feat: adds validateWebhook function

--- src/resources/webhook.js
+++ src/resources/webhook.js
@@ -1,5 +1,8 @@
 import T from 'proptypes';
+
 import base from './base';
+
+const crypto = require('crypto');
 
 export const propTypes = {
   id: T.string,
@@ -19,4 +22,38 @@ export default (api) =>
     static key = 'webhook';
 
     static propTypes = propTypes;
+
+    static validateWebhook(eventBody, headers, webhookSecret) {
+      let webhook = {};
+      const easypostHmacSignature = headers['X-Hmac-Signature'] ?? null;
+
+      if (easypostHmacSignature != null) {
+        const normalizedSecret = webhookSecret.normalize('NFKD');
+        const encodedSecret = Buffer.from(normalizedSecret, 'utf8');
+
+        const expectedSignature = crypto
+          .createHmac('sha256', encodedSecret)
+          .update(eventBody, 'utf-8')
+          .digest('hex');
+
+        const digest = `hmac-sha256-hex=${expectedSignature}`;
+
+        if (
+          crypto.timingSafeEqual(
+            Buffer.from(easypostHmacSignature, 'utf8'),
+            Buffer.from(digest, 'utf8'),
+          )
+        ) {
+          webhook = JSON.parse(eventBody.toString());
+        } else {
+          throw new Error(
+            'Webhook received did not originate from EasyPost or had a webhook secret mismatch.',
+          );
+        }
+      } else {
+        throw new Error('Webhook received does not contain an HMAC signature.');
+      }
+
+      return webhook;
+    }
   };
