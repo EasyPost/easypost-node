cc2f3c76cddbf8aae835a6947af2e7026ff8394e
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-10-11 15:43:56 -0600

    fix: nested cassette scrubbing

--- test/helpers/setup_polly.js
+++ test/helpers/setup_polly.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-shadow */
 /* eslint-disable no-param-reassign */
 /* eslint-disable import/no-extraneous-dependencies */
 import NodeHttpAdapter from '@pollyjs/adapter-node-http';
@@ -11,6 +13,22 @@ Polly.register(FSPersister);
 Polly.register(NodeHttpAdapter);
 
 const headerScrubbers = ['authorization', 'user-agent'];
+
+const redactedString = '<REDACTED>';
+const redactedObject = {};
+const redactedArray = [];
+const scrubbers = {
+  api_keys: redactedArray,
+  client_ip: redactedString,
+  credentials: redactedObject,
+  email: redactedString,
+  fields: redactedArray,
+  key: redactedString,
+  keys: redactedArray,
+  phone_number: redactedString,
+  phone: redactedString,
+  test_credentials: redactedObject,
+};
 
 function startPolly() {
   setupPolly({
@@ -38,42 +56,52 @@ function scrubHeaders(recording) {
   );
 }
 
-// Scrub sensitive data from response bodies (at the root level or in a root list)
-// prior to recording the cassette.
-// This function DOES NOT scrub data in nested objects or lists.
+/**
+ * Scrub individual element data of a cassette.
+ * @param {*} data
+ * @param {*} scrubberEntry
+ */
+function scrubData(data, scrubberEntry) {
+  const [key, replacement] = scrubberEntry;
+
+  // Root-level list scrubbing
+  if (Array.isArray(data)) {
+    data.map((item, index) => {
+      if (item[key]) {
+        data[index][key] = replacement;
+      }
+    });
+  } else if (typeof data === 'object' && data !== null) {
+    // Root-level key scrubbing
+    if (data[key]) {
+      data[key] = replacement;
+    } else {
+      // Nested scrubbing
+      Object.keys(data).forEach((item) => {
+        const element = data[item];
+        if (Array.isArray(element)) {
+          element.map((nestedItem, nestedIndex) => {
+            data[item][nestedIndex] = scrubData(nestedItem, scrubberEntry);
+          });
+        } else if (typeof element === 'object' && element !== null) {
+          data[item] = scrubData(element, scrubberEntry);
+        }
+      });
+    }
+  }
+
+  return data;
+}
+
+// Scrub sensitive data from response bodies prior to recording the cassette.
 function scrubResponseBodies(recording) {
-  const redactedString = '<REDACTED>';
-  const redactedObject = {};
-  const redactedArray = [];
-  const scrubbers = {
-    api_keys: redactedArray,
-    children: redactedArray,
-    client_ip: redactedString,
-    credentials: redactedObject,
-    email: redactedString,
-    key: redactedArray,
-    keys: redactedArray,
-    phone_number: redactedString,
-    phone: redactedString,
-    test_credentials: redactedObject,
-  };
   let response = recording.response.content.text;
 
   if (response) {
-    response = JSON.parse(response);
+    const responseBody = JSON.parse(response);
 
     Object.entries(scrubbers).forEach((scrubberEntry) => {
-      const [scrubber, scrubberValue] = scrubberEntry;
-      if (Array.isArray(response)) {
-        response = response.map((item) => {
-          if (item[scrubber]) {
-            item[scrubber] = scrubberValue;
-          }
-          return item;
-        });
-      } else if (response[scrubber]) {
-        response[scrubber] = scrubberValue;
-      }
+      response = scrubData(responseBody, scrubberEntry);
     });
   }
 
@@ -88,7 +116,7 @@ function setupCassette(server) {
       throw new Error(`Error decoding cassette: ${err.message}`);
     }
 
-    // TODO: Add support to scrub CC details from the request URL
+    // TODO: Add support to scrub CC details from the request URL and `queryParams`
 
     scrubHeaders(recording);
     try {
