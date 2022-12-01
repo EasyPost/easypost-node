2e9118b6509018c44c5bcbcd28a5cc9061cfcf07
nharris <nharris@easypost.com>
2022-12-01 17:20:19 -0500

    - Address feedback

--- test/resources/billing.test.js
+++ test/resources/billing.test.js
@@ -1,86 +1,78 @@
 /* eslint-disable func-names */
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 import EasyPost from '../../src/easypost';
-import * as setupPolly from '../helpers/setup_polly';
-import {MockMiddleware, MockRequest, MockRequestMatchRule, MockRequestResponseInfo} from "../helpers/mocking";
-
+import {
+  MockMiddleware,
+  MockRequest,
+  MockRequestMatchRule,
+  MockRequestResponseInfo,
+} from '../helpers/mocking';
 
 const middleware = (request) => {
-    return new MockMiddleware(request, [
-        new MockRequest(
-            new MockRequestMatchRule('POST', 'v2\\/bank_accounts\\/\\S*\\/charges$'),
-            new MockRequestResponseInfo(200, {})
-        ),
-        new MockRequest(
-            new MockRequestMatchRule('POST', 'v2\\/credit_cards\\/\\S*\\/charges$'),
-            new MockRequestResponseInfo(200, {})
-        ),
-        new MockRequest(
-            new MockRequestMatchRule('DELETE', 'v2\\/bank_accounts\\/\\S*$'),
-            new MockRequestResponseInfo(200, {})
-        ),
-        new MockRequest(
-            new MockRequestMatchRule('DELETE', 'v2\\/credit_cards\\/\\S*$'),
-            new MockRequestResponseInfo(200, {})
-        ),
-        new MockRequest(
-            new MockRequestMatchRule('GET', 'v2\\/payment_methods$'),
-            new MockRequestResponseInfo(200, {
-                "id": "summary_123",
-                "primary_payment_method": {
-                    "id": "card_123",
-                    "last4": "1234",
-                },
-                "secondary_payment_method": {
-                    "id": "bank_123",
-                    "bank_name": "Mock Bank",
-                }
-            })
-        )
-    ]);
-}
+  return new MockMiddleware(request, [
+    new MockRequest(
+      new MockRequestMatchRule('POST', 'v2\\/bank_accounts\\/\\S*\\/charges$'),
+      new MockRequestResponseInfo(200, {}),
+    ),
+    new MockRequest(
+      new MockRequestMatchRule('POST', 'v2\\/credit_cards\\/\\S*\\/charges$'),
+      new MockRequestResponseInfo(200, {}),
+    ),
+    new MockRequest(
+      new MockRequestMatchRule('DELETE', 'v2\\/bank_accounts\\/\\S*$'),
+      new MockRequestResponseInfo(200, {}),
+    ),
+    new MockRequest(
+      new MockRequestMatchRule('DELETE', 'v2\\/credit_cards\\/\\S*$'),
+      new MockRequestResponseInfo(200, {}),
+    ),
+    new MockRequest(
+      new MockRequestMatchRule('GET', 'v2\\/payment_methods$'),
+      new MockRequestResponseInfo(200, {
+        id: 'summary_123',
+        primary_payment_method: {
+          id: 'card_123',
+          last4: '1234',
+        },
+        secondary_payment_method: {
+          id: 'bank_123',
+          bank_name: 'Mock Bank',
+        },
+      }),
+    ),
+  ]);
+};
 
 describe('Billing Resource', function () {
-    setupPolly.startPolly();
 
-    before(function () {
-        this.easypost = new EasyPost(process.env.EASYPOST_PROD_API_KEY);
+  before(function () {
+    this.easypost = new EasyPost(process.env.EASYPOST_TEST_API_KEY, {
+      requestMiddleware: middleware,
     });
+  });
 
-    beforeEach(function () {
-        const {server} = this.polly;
-        setupPolly.setupCassette(server);
-    });
+  beforeEach(function () {
+  });
 
-    // eslint-disable-next-line jest/no-disabled-tests
-    it('fund a wallet', async function () {
-        this.easypost = new EasyPost(process.env.EASYPOST_TEST_API_KEY, {requestMiddleware: middleware});
+  it('fund a wallet', async function () {
+    const fund = await this.easypost.Billing.fundWallet('2000', 'primary');
 
-        // Skipping due to the lack of an available real payment method in tests
-        const fund = await this.easypost.Billing.fundWallet('2000', 'primary');
+    expect(fund).to.equal(true);
+  });
 
-        expect(fund).to.equal(true);
-    });
+  it('delete a payment method', async function () {
+    // Skipping due to the lack of an available real payment method in tests
+    const deletedPaymentMethod = await this.easypost.Billing.deletePaymentMethod('primary');
 
-    // eslint-disable-next-line jest/no-disabled-tests
-    it('delete a payment method', async function () {
-        this.easypost = new EasyPost(process.env.EASYPOST_TEST_API_KEY, {requestMiddleware: middleware});
+    expect(deletedPaymentMethod).to.equal(true);
+  });
 
-        // Skipping due to the lack of an available real payment method in tests
-        const deletedPaymentMethod = await this.easypost.Billing.deletePaymentMethod('primary');
+  it('retrieves all payment methods', async function () {
+    // Skipping due to the lack of an available real payment method in tests
+    const paymentMethods = await this.easypost.Billing.retrievePaymentMethods();
 
-        expect(deletedPaymentMethod).to.equal(true);
-    });
-
-    // eslint-disable-next-line jest/no-disabled-tests
-    it('retrieves all payment methods', async function () {
-        this.easypost = new EasyPost(process.env.EASYPOST_TEST_API_KEY, {requestMiddleware: middleware});
-
-        // Skipping due to the lack of an available real payment method in tests
-        const paymentMethods = await this.easypost.Billing.retrievePaymentMethods();
-
-        expect(paymentMethods.primary_payment_method).to.exist;
-        expect(paymentMethods.secondary_payment_method).to.exist;
-    });
+    expect(paymentMethods.primary_payment_method).to.exist;
+    expect(paymentMethods.secondary_payment_method).to.exist;
+  });
 });
