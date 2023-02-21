4eaae38aa614a57ec3ef7381e8bd2758b3a35186
jchen293 <jchen@easypost.com>
2023-02-21 13:40:25 -0500

    fix the param

--- src/exceptions/API/api_exception.js
+++ src/exceptions/API/api_exception.js
@@ -2,7 +2,7 @@ import EasyPostException from '../easypost_exception';
 
 export default class ApiException extends EasyPostException {
   constructor({ message, code, statusCode, errors } = {}) {
-    super(message);
+    super({ message });
     this.code = code;
     this.errors = errors;
     this.message = message;
