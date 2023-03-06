54991a58fcd737348fc0b572f588297510aa88e1
Nate Harris <nwithan8@users.noreply.github.com>
2023-03-06 15:19:42 -0700

    - Add missing "status_detail" property for Tracker class (automagically included for TrackerDetail object) (#359)
    - Update TypeScript definitions

---
+++ types/Tracker/TrackerStatusDetail.d.ts
@@ -0,0 +1,7 @@
+export declare type ITrackerStatusDetail =
+  | 'unknown'
+  | 'status_update'
+  | 'departed_facility'
+  | 'arrived_at_facility'
+  | 'out_for_delivery'
+  | 'arrived_at_destination';
