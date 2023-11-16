e027e378f4ffa01e2ca9eb59a608247219ddff9f
Alex Matthews <amatthews@easypost.com>
2023-11-16 15:21:02 -0600

    Removes nested ternary

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -7,21 +7,29 @@
 export const withoutParams = (obj) =>
   Object.fromEntries(
     Object.entries(obj)
-      .map(([key, value]) =>
-        key === '_params'
-          ? null
-          : !value
-          ? [key, value]
-          : Array.isArray(value)
-          ? [
-              key,
-              value.map((arrValue) =>
-                typeof arrValue === 'object' ? withoutParams(arrValue) : arrValue,
-              ),
-            ]
-          : typeof value === 'object'
-          ? [key, withoutParams(value)]
-          : [key, value],
-      )
+      .map(([key, value]) => {
+        if (key === '_params') {
+          return null;
+        }
+
+        if (!value) {
+          return [key, value];
+        }
+
+        if (Array.isArray(value)) {
+          return [
+            key,
+            value.map((arrValue) =>
+              typeof arrValue === 'object' ? withoutParams(arrValue) : arrValue,
+            ),
+          ];
+        }
+
+        if (typeof value === 'object') {
+          return [key, withoutParams(value)];
+        }
+
+        return [key, value];
+      })
       .filter(Boolean),
   );
