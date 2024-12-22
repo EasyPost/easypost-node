bebeaea9c7a0e441d8cb7744d757cc985bc51896
Alex Matthews <ralexmatthews@gmail.com>
2024-12-22 11:38:18 -0600

    Cleans up config a bit

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,35 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import { viteExternalsPlugin } from 'vite-plugin-externals';
 
-export default defineConfig(({ mode }) => {
-  const isDev = mode === 'development';
+const mode = process.env.NODE_ENV || 'development';
+const isDev = mode === 'development';
 
-  return {
-    build: {
-      lib: {
-        entry: path.resolve(__dirname, 'src/easypost.js'),
-        name: 'easypost',
-        fileName: 'easypost',
-        formats: ['cjs'],
-      },
-      outDir: 'dist',
-      sourcemap: isDev,
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/easypost.js'),
+      fileName: 'easypost',
+      formats: ['cjs'],
     },
-    plugins: [
-      viteExternalsPlugin({
-        type: 'commonjs',
-        externals: ['node'],
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
+    sourcemap: isDev,
+    rollupOptions: {
+      external: [/^node:.*/, /^@?[a-zA-Z\-_]+\/?[a-zA-Z\-_]*$/],
+      output: {
+        dir: 'dist',
       },
     },
-  };
+  },
+
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  mode,
+
+  optimizeDeps: {
+    exclude: [],
+  },
 });
