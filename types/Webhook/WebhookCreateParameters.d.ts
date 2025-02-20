038f0c10edcec27b285cc640e1cd911756b26ea4
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-02-20 10:09:46 -0700

    fix: type of custom_headers

--- types/Webhook/WebhookCreateParameters.d.ts
+++ types/Webhook/WebhookCreateParameters.d.ts
@@ -1,5 +1,5 @@
 export declare interface IWebhookCreateParameters {
   url: string;
   secret?: string;
-  custom_headers?: Record<string, string>;
+  custom_headers?: Array<{ key: string; value: string }>;
 }
