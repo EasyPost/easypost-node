34eeaef095036480b15b37d9906f47b4960f40ea
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-11-18 11:54:59 -0700

    feat: add typescript definitions for customer portal and embeddable sessions

---
+++ types/CustomerPortal/CustomerPortalAccountLink.d.ts
@@ -0,0 +1,38 @@
+import { ICustomerPortalAccountLinkCreateParameters } from './CustomerPortalAccountLinkCreateParameters';
+
+/**
+ * Class representing an EasyPost CustomerPortalAccountLink object.
+ *
+ */
+export declare interface ICustomerPortalAccountLink {
+  /** Always returns "CustomerPortalAccountLink". */
+  object: string;
+
+  /** One-time-use session URL for initiating the Customer Portal. */
+  link: string;
+
+  /** One-time-use session URL for initiating the Customer Portal. */
+  created_at: string;
+
+  /** ISO 8601 timestamp when the link will expire (5 minutes from creation). */
+  expires_at: string;
+}
+
+export declare class CustomerPortalAccountLink implements ICustomerPortalAccountLink {
+  public constructor(input: ICustomerPortalAccountLinkCreateParameters);
+
+  object: 'CustomerPortalAccountLink';
+  link: string;
+  created_at: string;
+  expires_at: string;
+
+  /**
+   * Create a Portal Session.
+   *
+   * @see https://docs.easypost.com/docs/customer-portals#creating-a-portal-session
+   *
+   * @param {Object} params The parameters to create a {@link CustomerPortalAccountLink} with.
+   * @returns {Promise<CustomerPortalAccountLink>} The {@link CustomerPortalAccountLink}.
+   */
+  static createAccountLink(params: Object): Promise<CustomerPortalAccountLink>;
+}
