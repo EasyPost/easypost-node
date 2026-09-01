9e75755cec0bdaa6fbd7a1275789b82514a2e548
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2026-09-01 15:10:34 -0600

    chore: remove unused deps, bump remaining

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ const isDev = mode === 'development';
 
 export default defineConfig({
   build: {
-    // node 12 is currently the oldest version of node we support. If we want to
-    // drop node 12 support in the future, change this to the next min version
-    target: 'node12',
+    // This target must match the oldest version we support
+    target: 'node18',
     lib: {
       entry: path.resolve(__dirname, 'src/easypost.ts'),
       fileName: 'easypost',
